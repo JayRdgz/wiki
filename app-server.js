9ef9a25de191e05701feaa8a2e8374a5a2e45213
JayRdgz <cvjersson.rp91@gmail.com>
2017-09-10 21:34:32 -0400

    third-commit-files-added

--- app-server.js
+++ app-server.js
@@ -1,1 +1,27 @@
 'use strict'
+const express = require('express')
+
+let app = express()
+
+app.get('/', function(req, res) {
+    res.render('index', {
+        title: 'Wiki - Node based',
+        message: 'Hello there'
+    })
+})
+
+const keyhint = []
+const secretCode = "getmeaccess"
+
+// # Listener for the events inside the DOM
+window.addEventListener('keyup', function(e){
+
+    // # Insert the key values
+    keyhint.push(e.key)
+    console.log(keyhint)
+
+    // # validates de keys of the given values from the client side
+    if ( keyhint.join('').includes(secretCode) )
+        alert('Hello world')
+
+})
